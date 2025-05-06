2c2081d1e379d6534b9808536ae2ec84238e2a34
Leonardo16Rojas <leoabsalonrojas72@gmail.com>
2025-05-05 21:13:09 -0500

    Calculator Python.py

--- Corte1/PythonClasses/Calculator Python.py
+++ Corte1/PythonClasses/Calculator Python.py
@@ -14,18 +14,19 @@ class Calculator:#Python usa class Calcualtor: en lugar de public class Calculat
     def multiply(self, a, b): # Método para multiplicar dos números
         return a * b
     
-    def divide(self, a, b):  # Método para dividir dos números
-            if b == 0:
-             raise ValueError("No se puede dividir por cero")
-            return a / b
-        # Nota: En Python la división de enteros con / devuelve float
-        # Si quisieras división entera como en Java, deberías usar //  (return a // b)
-      
-    
-    def modulo(self, a, b):  # Método para obtener el módulo
-        if b == 0:
-          raise ValueError("No se puede calcular el módulo por cero")
-        return a % b
+    def divide(self, a, b): # Metodo para dividir dos numeros
+        try:
+           return a / b # Nota: En Python la division de enteros con / devuelve float
+        except ZeroDivisionError:
+           return "No se puede dividir entre cero"
+    # Si quisiera la division entera como en Java, deberias usar //  (return a // b)
+       
+
+    def modulo(self, a, b):  # Metodo para obtener el modulo
+        try:
+          return a % b
+        except ZeroDivisionError:
+            return "No se puede calcular el módulo por cero"
 
   #Python usa if __name__ == "__main__": para el punto de entrada
 if __name__ == "__main__": # El equivalente al main de Java en Python es este bloque if,
@@ -35,5 +36,5 @@ if __name__ == "__main__": # El equivalente al main de Java en Python es este bl
     print(my_calculator.add(5, 7)) #No se declara el tipo de variable
     print(my_calculator.substract(45, 11)) #No se usa new, solo se llama a la clase como si fuera una función
     print(my_calculator.multiply(3, 2))
-    print(my_calculator.divide(12,24))
+    print(my_calculator.divide(12,0))
     print(my_calculator.divide(12,47))
